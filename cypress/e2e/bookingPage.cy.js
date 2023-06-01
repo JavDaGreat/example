90b86f5c678cd34d2d1671971a1bc84a02e54bea
Javad <1998javad@gmail.com>
2023-06-01 14:36:59 +0200

    yey

--- cypress/e2e/bookingPage.cy.js
+++ cypress/e2e/bookingPage.cy.js
@@ -30,32 +30,4 @@ describe('Booking Page', () => {
     cy.get('[class="button booking__button"]')
   })
 
-
-  
-
-
-
 })
-
-
-
-    //  cy.wait(500)
-    //  cy.get("[class='shoes__button shoes__button--small']").eq(4).click()
-    // cy.wait(200)
-    // cy.get('[class="button booking__button"]').click()
-    // cy.wait(1000)
-    // cy.get("[class='input__field confirmation__input']").eq(0).should('have.value','2021-01-20 18:00')
-    // cy.get("[class='input__field confirmation__input']").eq(3).invoke('val').should('not.be.empty')
-    // cy.get("[class='confirmation__price'] p:nth-child(2)").should('have.text','680 sek')
-    // cy.get("[class='navigation__icon']").click()
-    // cy.wait(100)
-    // cy.get(".navigation__link").eq(0).click()
-    
-    // cy.wait(1000)
-    // cy.get("[class='navigation__icon']").click()
-    // cy.get(".navigation__link").eq(1).click()
-
-
-
-
-    
